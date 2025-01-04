3c0ddd38eb0c65b10802d8a9b3cac3c04cc6b451
akadot <muriloholiveira@hotmail.com>
2025-01-04 20:13:41 -0300

    💄 Styling posts gallery

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 type PostStyleProps = {
     cover_img:string,
-    color:string
+    color:string,
 }
 
 const fetcher = async (url: string) => await fetch(url).then((res) => {
@@ -16,7 +16,7 @@ const fetcher = async (url: string) => await fetch(url).then((res) => {
 
 export default function Posts() {
     const { data, error, isLoading } = useSWR(
-        "https://dev.to/api/articles?username=akadot_",
+        "https://dev.to/api/articles?username=olidev_",
         fetcher
     );
 
@@ -27,11 +27,16 @@ export default function Posts() {
         <section>
             <h1>POSTS</h1>
             <PostContainer>
-                {data && data.map((post: { id: number, cover_image:string }) => {
+                {data && data.map((post: { id: number, cover_image:string, title:string, tag_list:string[] }) => {
                     return (
-                        <PostItem cover_img={post.cover_image} color="purple"> 
-                            <p>Post title</p> 
-                            <span>Description trestesd fdsoçjifnaçsdfngdk vakdfnvçsdilHJGAFDHGASDFLJFVSÇL</span> 
+                        <PostItem cover_img={post.cover_image} color="purple">
+                            <section>
+                                <p>{post.title}</p>
+                                {post.tag_list.map((tag, idx) => <span key={'tag'+idx} >{tag}</span>)}
+                            </section>
+                            <img src={post.cover_image} alt="abc" />
+                            
+                            
                             {/* <img src={post.cover_image} alt="cover" /> */}
                             {/* <Article articleId={post.id} /> */}
                         </PostItem>
@@ -43,21 +48,50 @@ export default function Posts() {
 }
 
 const PostContainer = styled.section`
-    display:grid;
-    grid-template-columns: 1fr 1fr;
-    column-gap:1rem;
+    display:flex;
+    flex-direction:column;
     row-gap: 1rem;
+    margin-top: 2rem;
 `;
 
 const PostItem = styled.section<PostStyleProps>`
-    background-image: url(${props => props.cover_img});
-    width:100%;
-    height:15rem;
+    cursor: pointer;
+    display: flex;
+    align-items:center;
+    justify-content:space-between;
+    padding:0.8rem 1rem;
     border-radius:0.8rem;
-    /* box-shadow: ${props => props.color} 0px 8px 24px; */
-    box-shadow: #0b0331 0px 3px 8px;
-    /* box-shadow: ${props => props.color} 0px 4px 16px, ${props => props.color} 0px 8px 32px; */
+    background-color: rgba(68, 68, 68, 0.13);
+    border: 1px solid #8b8b8b11;
 
+    & img{
+        width:5rem;
+        height:5rem;
+        border-radius:0.5rem;
+        object-fit:cover;
+        border:2px solid #50fa7a1f;
+    }
+
+    & p{
+        font-weight:bold;
+        font-size:1.2rem;
+        margin-bottom:0.5rem;
+    }
+
+    & span{
+        margin-right:0.3rem;
+        font-size:0.7rem;
+        padding:0.3rem 0.5rem;
+        border-radius: 50rem;
+        background-color: #50fa7a1f;
+        color: #50fa7a;
+    }
+
+    &:hover p{
+        color:#50fa7a88;
+    }
+
+     &:hover{
+        border-color:#50fa7a1f;
+     }
 `;
-
-//IDEA https://dribbble.com/shots/20802507-Blog-posts
